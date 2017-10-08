8b62b048cc7aa1ea07510835ee2b0a00d68baa17
rajeeviiit <singhrajiv774@gmail.com>
2017-10-08 12:23:29 +0530

    index file added

--- Nodejs/server.js
+++ Nodejs/server.js
@@ -1,9 +1,18 @@
 var http = require('http');
-
+var fs  =require('fs');
 function onRequest(request, response){
 
-response.writeHead(200,{'Content-Type':'text/plain'});
-response.write('Hello World');
-response.end();
+response.writeHead(200,{'Content-Type':'text/html'});
+fs.readFile('./index.html',null,function(error,data){
+if(error){
+          response.writeHead(404);
+          response.writeHead('File not Found');
+        }else{
+
+          response.write(data);
+        }
+        response.end();
+});
+
 }
 http.createServer(onRequest).listen(8000);
